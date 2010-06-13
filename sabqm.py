b4133d69dd069484c1fd0bcafdfd40cf8425e28a
Dustin Wyatt <dustin.wyatt@gmail.com>
2010-06-13 11:11:53 -0500

    fixed syntax error from previous commit

--- sabqm.py
+++ sabqm.py
@@ -129,7 +129,7 @@ while 1:
         if nzb:
             screen_log("Adding %s" % os.path.basename(nzb))
             try:
-                screen_log(sab_add_by_path(url, port, apikey, nzb, sab_category)
+                screen_log(sab_add_by_path(url, port, apikey, nzb, sab_category))
             except:
                 screen_log("failed to add %s" % os.path.basename(nzb))
                 shutil.move(nzb, nzb + ".fail")
